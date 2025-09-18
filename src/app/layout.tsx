be8c8574ed36dc0a5813149b49f5cc20a666ce23
alexmatsuo <alexmatsuo@hotmail.com>
2025-09-17 21:20:24 -0300

    fix bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-black text-[#D3CEAD] antialiased font-sans flex flex-col min-h-screen">
+        {/* Background container that adapts to viewport */}
         <div 
-          className="fixed inset-0 z-[-1] simple-background"
+          className="fixed inset-0 z-[-1] w-full h-full"
           style={{
             backgroundImage: "url('/background.jpg')",
+            backgroundSize: 'cover',
+            backgroundPosition: 'center center',
+            backgroundRepeat: 'no-repeat',
+            minHeight: '100vh',
+            minWidth: '100vw',
           }}
         >
           {/* Dark overlay */}
@@ -27,7 +33,7 @@ export default function RootLayout({
         </div>
         
         <ConditionalHeader />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 relative z-0">{children}</main>
         <Footer />
       </body>
     </html>
