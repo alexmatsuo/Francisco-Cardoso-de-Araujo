521b414d41033943c3ef8df0fde7e78c09a8b3f5
alexmatsuo <alexmatsuo@hotmail.com>
2026-03-03 15:23:34 -0300

    fix animation bio

--- src/components/Disclose.tsx
+++ src/components/Disclose.tsx
@@ -2,25 +2,13 @@ import { ImgHTMLAttributes, useState } from "react";
 
 import { cn } from "@/lib/utils";
 
-/**
- * All the props are passed to the img element.
- * Make sure to adjust the width and height of the container div
- * as per the design requirements/image aspect ratio.
- */
 export default function DiscloseImage({
   className,
   doorClassName,
   vertical = false,
   ...props
 }: ImgHTMLAttributes<HTMLImageElement> & {
-  /**
-   * Class name for the window on the left and right side of the image.
-   */
   doorClassName?: string;
-
-  /**
-   * If true, the doors will slide vertically.
-   */
   vertical?: boolean;
 }) {
   const [imageLoaded, setImageLoaded] = useState(false);
@@ -29,7 +17,6 @@ export default function DiscloseImage({
 
   return (
     <div className="relative overflow-hidden">
-      {/* Use `next/image` and remove the line below. */}
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         alt=""
@@ -40,17 +27,29 @@ export default function DiscloseImage({
 
       {imageLoaded && (
         <>
+          {/* First door: left (horizontal) or top (vertical) */}
           <div
             className={cn(baseClassName, doorClassName, {
-              "top-0 h-1/2 w-full slide-out-to-top-full": vertical,
-              "bottom-0 left-0 top-0 w-1/2 slide-out-to-left-full": !vertical,
+              "slide-out-to-top-full": vertical,
+              "slide-out-to-left-full": !vertical,
             })}
+            style={
+              vertical
+                ? { top: 0, left: 0, right: 0, height: "calc(50% + 1px)" }
+                : { top: "-1px", bottom: "-1px", left: 0, width: "50%" }
+            }
           />
+          {/* Second door: right (horizontal) or bottom (vertical) */}
           <div
             className={cn(baseClassName, doorClassName, {
-              "bottom-0 h-1/2 w-full slide-out-to-bottom-full": vertical,
-              "bottom-0 right-0 top-0 w-1/2 slide-out-to-right-full": !vertical,
+              "slide-out-to-bottom-full": vertical,
+              "slide-out-to-right-full": !vertical,
             })}
+            style={
+              vertical
+                ? { bottom: 0, left: 0, right: 0, height: "calc(50% + 1px)", top: "calc(50% - 1px)" }
+                : { top: "-1px", bottom: "-1px", right: 0, width: "50%" }
+            }
           />
         </>
       )}
